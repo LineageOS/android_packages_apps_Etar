af09c054d16502c5bbc1ff76f58b140fc937cdbf
Aayush Gupta <aayushgupta219@gmail.com>
2022-06-27 21:05:46 +0530

    Etar: Request to disable battery optimizations
    Required to ensure notifications and service works as intended.
    
    Show an info menu item on toolbar if optimizations are enabled to
    attract user's attention to disable it.
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I540028aa44b47b7353582168f85f43679b931572

--- src/com/android/calendar/AllInOneActivity.java
+++ src/com/android/calendar/AllInOneActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2022 The Calyx Institute
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +34,7 @@ import android.content.AsyncQueryHandler;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -46,9 +48,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.PowerManager;
 import android.provider.CalendarContract;
 import android.provider.CalendarContract.Attendees;
 import android.provider.CalendarContract.Events;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.text.format.DateUtils;
@@ -397,6 +401,21 @@ public class AllInOneActivity extends AbstractCalendarActivity implements EventH
         }
     }
 
+    private void checkAndRequestDisablingDoze() {
+        if (!dozeDisabled()) {
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+            startActivity(intent);
+        }
+    }
+
+    private Boolean dozeDisabled() {
+        String packageName = getApplicationContext().getPackageName();
+        PowerManager pm = (PowerManager) getApplicationContext().getSystemService(Context.POWER_SERVICE);
+        return pm.isIgnoringBatteryOptimizations(packageName);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
@@ -407,6 +426,9 @@ public class AllInOneActivity extends AbstractCalendarActivity implements EventH
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                     // permission was granted, yay!
+
+                    // Check and ask to disable battery optimizations
+                    checkAndRequestDisablingDoze();
 
                 } else {
                     Toast.makeText(getApplicationContext(), R.string.user_rejected_calendar_write_permission, Toast.LENGTH_LONG).show();
@@ -845,6 +867,14 @@ public class AllInOneActivity extends AbstractCalendarActivity implements EventH
         LayerDrawable icon = (LayerDrawable) menuItem.getIcon();
         Utils.setTodayIcon(icon, this, mTimeZone);
 
+        // Handle warning for disabling battery optimizations
+        if (dozeDisabled()) {
+            MenuItem menuInfoItem = menu.findItem(R.id.action_info);
+            if (menuInfoItem != null) {
+                menuInfoItem.setVisible(false);
+            }
+        }
+
         return true;
     }
 
@@ -925,6 +955,8 @@ public class AllInOneActivity extends AbstractCalendarActivity implements EventH
             Intent intent = new Intent(this, SettingsActivity.class);
             intent.putExtra(SettingsActivityKt.EXTRA_SHOW_FRAGMENT, ViewDetailsPreferences.class.getName());
             startActivity(intent);
+        } else if (itemId == R.id.action_info) {
+            checkAndRequestDisablingDoze();
         } else {
                 return mExtensions.handleItemSelected(item, this);
         }
