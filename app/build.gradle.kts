725cc98a73324a09b7ed460d4322adb05a5af3d5
LuK1337 <priv.luk@gmail.com>
2025-06-29 23:16:36 +0200

    Etar: Setup AOSP build
    Co-authored-by: Aayush Gupta <aayushgupta219@gmail.com>
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Co-authored-by: Tommy Webb <tommy@calyxinstitute.org>
    Change-Id: Iceb57c60ebc38db9553beb5e4dc2b8caace49977

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,13 @@
+import com.android.build.gradle.internal.tasks.factory.dependsOn
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.lineageos.generatebp.GenerateBpPluginExtension
+import org.lineageos.generatebp.models.Module
 
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.ec4j.editorconfig)
+    alias(libs.plugins.lineageos.generatebp)
 }
 
 editorconfig {
@@ -139,4 +143,18 @@ dependencies {
 
 	// lifecycle
 	implementation(libs.androidx.lifecycle.livedata)
+}
+
+configure<GenerateBpPluginExtension> {
+	targetSdk.set(android.defaultConfig.targetSdk!!)
+	minSdk.set(android.defaultConfig.minSdk!!)
+	availableInAOSP.set { module: Module ->
+		when {
+			module.group.startsWith("androidx.databinding") -> false
+			module.group.startsWith("androidx") -> true
+			module.group.startsWith("com.google") -> true
+			module.group.startsWith("org.jetbrains") -> true
+			else -> false
+		}
+	}
 }
